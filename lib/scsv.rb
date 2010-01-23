dc6939df9f03fe29bd340da467645dc78d13f821
kinumi <kunimi.ikeda@gmail.com>
2010-01-23 15:16:10 +0900

    0.2.0

--- lib/scsv.rb
+++ lib/scsv.rb
@@ -5,7 +5,7 @@ $:.unshift(File.dirname(__FILE__)) unless
 # for COMMA separated file
 #
 class SCSV  
-  VERSION = '0.1.0'
+  VERSION = '0.2.0'
   
   DEFAULT_OPTIONS = {
     :col_sep  => ",",
--- test/test_scsv.rb
+++ test/test_scsv.rb
@@ -8,56 +8,44 @@ class TestAbc < Test::Unit::TestCase
   def test_parse_csv_with_block
     SCSV.parse("test/_data/csv_has_header.txt") do |row|
       assert_instance_of Hash, row
-      p row
     end
 
     SCSV.parse("test/_data/csv_has_no_header.txt", {:header => false}) do |row|
       assert_instance_of Array, row
-      p row
     end
 
     SCSV.parse("test/_data/csv_has_no_header.txt", {:header => ["h1", "h2", "h3"]}) do |row|
       assert_instance_of Hash, row
-      p row
     end
 
     SCSV.parse("test/_data/csv_japanese_utf8.txt") do |row|
       assert_instance_of Hash, row
-      p row
     end
 
     SCSV.parse("test/_data/tsv.txt", {:col_sep => "\t"}) do |row|
       assert_instance_of Hash, row
-      p row
     end
     STSV.parse("test/_data/tsv.txt") do |row|
       assert_instance_of Hash, row
-      p row
     end
   end
   
   def test_parse_csv_with_no_block
     csv = SCSV.parse("test/_data/csv_has_header.txt")
     assert_instance_of Array, csv
-    p csv
 
     csv = SCSV.parse("test/_data/csv_has_no_header.txt", {:header => false})
     assert_instance_of Array, csv
-    p csv
 
     csv = SCSV.parse("test/_data/csv_has_no_header.txt", {:header => ["h1", "h2", "h3"]})
     assert_instance_of Array, csv
-    p csv
 
     csv = SCSV.parse("test/_data/csv_japanese_utf8.txt")
     assert_instance_of Array, csv
-    p csv
 
     csv = SCSV.parse("test/_data/tsv.txt", {:col_sep => "\t"})
     assert_instance_of Array, csv
-    p csv
     csv = STSV.parse("test/_data/tsv.txt")
     assert_instance_of Array, csv
-    p csv
   end
 end
